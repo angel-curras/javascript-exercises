290b859f44b8e741973c446ac824f2638679a79f
Angel Curras Sanchez <angel.curras@hm.edu>
2024-09-01 10:45:21 +0200

    Add task 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+
+    // (year % 4 === 0) && (year % 100 === 0) && (year % 400 === 0) || (year % 4 === 0) && (year % 100 !== 0)
+    // (year % 4 === 0) && ( (year % 100 === 0) && (year % 400 === 0)  || (year % 100 !== 0) )
+    // (year % 100 === 0) && (year % 400 === 0)  || (year % 100 !== 0)
+    // A && B || !A
+
+    // !!(A && B || !A)
+    // !(!(A && B ) & !!A)
+    // !( !(A && B ) & A)
+    // !( (!A || !B ) & A)
+    // !( (!A & A || !B & A )
+    // !( !B & A )
+    // B || !A )
+
+    return (year % 4 === 0) && ( (year % 400 === 0) || (year % 100 !== 0) )
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// 
