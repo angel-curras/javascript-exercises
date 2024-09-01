1ee30c3a436701233af6883f53b6f1d1c0c9cc6e
Angel Curras Sanchez <angel.curras@hm.edu>
2024-09-01 10:50:23 +0200

    Add task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  let celsius = (temperatureFahrenheit - 32) * 5 / 9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  let fahrenheit = (temperatureCelsius * 9 / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
