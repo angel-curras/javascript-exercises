7114e6c3637d26ca287f55209287d6a9e7367ee7
Angel Curras Sanchez <angel.curras@hm.edu>
2024-09-01 10:23:37 +0200

    Add task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+function isIntegerNumber(value) {
+    return typeof value === 'number' && Number.isInteger(value);
+}
 
+const sumAll = function(first, second) {
+
+    if (!isIntegerNumber(first) || !isIntegerNumber(second)) {
+        return "ERROR";
+    }
+
+    if (first < 0 || second < 0) {
+        return "ERROR";
+    }
+
+    if (first === second) {
+        return 0;
+    }
+
+    let min = Math.min(first, second);
+    let max = Math.max(first, second);
+
+    let totalSum = 0;
+
+    for (let counter=min; counter <= max; counter++) {
+        totalSum += counter;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
