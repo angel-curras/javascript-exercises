1fa1771246d2f83d6aa69b8a49f840ecd4247a5c
Angel Curras Sanchez <angel.curras@hm.edu>
2024-09-01 09:55:29 +0200

    Add task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(text, repetition=1) {
+
+    if (repetition < 0) {
+        return "ERROR";
+    }
+
+    let result = "";
+    for (let count = 0; count < repetition; count++) {
+        result += text;
+    }
+
+    return result;
 
 };
 
