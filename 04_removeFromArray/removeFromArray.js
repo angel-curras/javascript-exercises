36a5a3a2ddbedbdd0c2396c9bf8f9628b5a4b7d9
Angel Curras Sanchez <angel.curras@hm.edu>
2024-09-01 10:10:47 +0200

    add task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...element) {
+
+    if (element === undefined) {
+        return arr;
+    }
+
+    for (const element_to_remove of element) {
+        arr = arr.filter(item => item !== element_to_remove);
+    }
+
+    return arr;
+
 };
 
 // Do not edit below this line
